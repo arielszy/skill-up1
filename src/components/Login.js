6db6446a1cb13fc48b7884436bd353b870e2f629
ariel <arielszy@yahoo.com.ar>
2022-05-24 23:42:11 -0300

    clase 3 - sweet alert

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,33 +1,33 @@
-import axios from "axios";//libreria para peticiones a la api
+import axios from "axios"; //libreria para peticiones a la api
+import swAlert from "@sweetalert/with-react";
 
 function Login() {
-
   const submitHandler = (e) => {
     e.preventDefault(); //evita que se recargue la pagina
     const regexEmail = /\S+@\S+\.\S+/; //expresion regular para verificar email
-    
+
     //captura los datos de quien disparo el evento(solo para formularios)
     const email = e.target.email.value;
     const password = e.target.password.value;
 
-    if (email === '' || password === '') {
-      console.log('los campos no pueden estar vacios');
-    } else if(!regexEmail.test(email)){//tambien se puee usar return (dentro del if) en vez de else if
-      console.log('debe ingresar un email valido');
-    } else if (email !== 'challenge@alkemy.org' || password !== 'react') {
-      console.log('credenciales inválidas');
+    if (email === "" || password === "") {
+      swAlert(<h3>los campos no pueden estar vacios</h3>); //muestra el alert. se usa jsx
+    } else if (!regexEmail.test(email)) {
+      //tambien se puee usar return (dentro del if) en vez de else if
+      swAlert(<h3>debe ingresar un email valido</h3>);
+    } else if (email !== "challenge@alkemy.org" || password !== "react") {
+      swAlert(<h3>credenciales inválidas</h3>);
     } else {
-      console.log('ingreso correcto');
       axios
-        .post('http://challenge-react.alkemy.org', { email, password })//envio a la api en formato('url del endpoint de la api',{datos a enviar})
-        .then(res => { console.log(res.data); })//luego de recibir la respuesta la muestra
+        .post("http://challenge-react.alkemy.org", { email, password }) //envio a la api en formato('url del endpoint de la api',{datos a enviar})
+        .then((res) => {
+          swAlert(<h3>ingreso correcto</h3>);
+          console.log(res.data);
+        }); //luego de recibir la respuesta la muestra
     }
-
-
-
-
   };
-  return (//jsx no html
+  return (
+    //jsx no html
     <>
       <h1>formulario de Login</h1>
       <br />
