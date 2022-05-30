dd5cd38f69c296c48214a9389d8e806aa44c2bf1
ariel <arielszy@yahoo.com.ar>
2022-05-29 22:52:20 -0300

    course is outdated. so many errors!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Listado from "./components/Listado";
 
 function App() {
   return (
     <>
-      <switch>
+      <Routes>
         <Route exact path="/" component={Login} />
-        //exactamente en en la raiz
         <Route path="/listado" component={Listado} />
-      </switch>
+      </Routes>
     </>
   );
 }
--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,14 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+// eslint-disable-next-line
 
 function Listado() {
   const Navigate = useNavigate();
-  useEffect(() => {
+  //useEffect(() => {
     const token = localStorage.getItem("token"); //recupera los datos de la memoria local
     if (!token) {
       Navigate("/");
     }
-  }, []);
+ // }, []);
 
   return <h1>Soy el componente listado</h1>;
 }
