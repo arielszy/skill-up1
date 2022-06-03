9515212ec76fd8ababaae4ceeb79f87876265ef8
ariel <arielszy@yahoo.com.ar>
2022-06-02 23:55:53 -0300

    clase 2 - footer

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+//rel="noopener noreferrer" recomendacion de react para link externos a la app.
+function Footer() {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a
+              href="http://www.google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              link
+            </a>
+          </li>
+          <li>
+            <p>copyright whatever</p>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
