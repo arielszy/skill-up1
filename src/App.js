3bc821f1e2f0e15b06579451a9ec1d76687bd2be
ariel <arielszy@yahoo.com.ar>
2022-06-03 00:21:17 -0300

    clase 3 - integrar header y footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+//custom components
 import Login from "./components/Login";
 import Listado from "./components/Listado";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route exact path="/" element={<Login />} />
         <Route path="/listado" element={<Listado />} />
       </Routes>
+      <Footer />
     </>
   );
 }
