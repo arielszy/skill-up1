8499ef46c020a19c5e0510e6b140832cf042a928
ariel <arielszy@yahoo.com.ar>
2022-06-02 23:40:12 -0300

    unidad 3 clase 1 - encabezado de navegacion

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Listado">Listado</Link>
+          </li>
+          <li>
+            <Link to="/Contacto">Contacto</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
